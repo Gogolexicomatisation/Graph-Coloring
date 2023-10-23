a5100e7ec6a66dfcd22aeb9583c1ad55c1f99d91
Hugo Weiss <hugo.weiss@epita.fr>
2023-10-23 19:09:18 +0200

    graph coloring functionnal, potential algorithm issue

--- classical_mis.py
+++ classical_mis.py
@@ -1,3 +1,6 @@
+from math import inf
+
+# Change a number to its binary string version
 def int_to_binary(n, size=0):
     if n == 0:
         return '0'.rjust(size, '0')
@@ -9,7 +12,7 @@ def int_to_binary(n, size=0):
         
     return binary.rjust(size, '0')
 
-
+# calculate the cost of a specific configuration
 def cost_function(bit_string, length, adj_list):
     cost = 0
     for i in range(length):
@@ -22,31 +25,76 @@ def cost_function(bit_string, length, adj_list):
                 cost += 2 * int(bit_string[i]) * int(bit_string[j])
                 
     return cost
+
+def have_common_bit(bit_string1, bit_string2):
+    # Convert bitstrings to integers
+    num1 = int(bit_string1, 2)
+    num2 = int(bit_string2, 2)
     
-    
-def combinatorial_optimization_MIS(adj_list, vertices):
+    # Check if the result of the AND operation is not zero
+    return (num1 & num2) != 0
+
+# return the cost function and the minimal cost configuration
+def combinatorial_optimization_MIS(adj_list, actual_vertices, vertices, colored_edges):
     cost_list = []
     for i in range(2**vertices):
         number = int_to_binary(i, vertices)
+        if have_common_bit(number, colored_edges):
+            cost_list.append(inf)
+            continue
         cost_list.append(cost_function(number, vertices, adj_list))
-    return int_to_binary(cost_list.index(min(cost_list)), vertices)
+    return int_to_binary(cost_list.index(min(cost_list)), vertices) #PROBLEMS?
 
-# def graph_coloring(adj_list, vertices):
+def remove_edges(new_adjacent_list, index):
+    return [t for t in new_adjacent_list if index not in t]
+
+
+def remove_vertices_edges(bit_string, vertices ,adjacent_list):
+    new_adj_list = adjacent_list.copy()
+    for i in range(len(bit_string)):
+        if bit_string[i] == '1':
+            new_adj_list = remove_edges(new_adj_list, i+1)
+            vertices -= 1
+    return new_adj_list, vertices
+
+def count_color(color_list, vertices):    
+    colored_edges = '0' * vertices
+    for elements in color_list:
+        colored_edges = int_to_binary(int(elements, 2) | int(colored_edges, 2), vertices)
+    return colored_edges
+
+def graph_coloring(adj_list, vertices):
+    
+    actual_vertices = vertices
+    color_list = []
+    colored_edges = count_color(color_list, vertices)
+    while adj_list != []:
+        mis_bit_string = combinatorial_optimization_MIS(adj_list, actual_vertices, vertices, colored_edges)
+        color_list.append(mis_bit_string)
+        adj_list, actual_vertices = remove_vertices_edges(mis_bit_string, actual_vertices, adj_list)
+        colored_edges = count_color(color_list, vertices)
+    
+    last_mis_bit_string = combinatorial_optimization_MIS(adj_list, actual_vertices, vertices, colored_edges)
+    color_list.append(last_mis_bit_string)
+    return color_list
 
 
 # adjacent_list = [(1,2), (2,3)]
 # vert = 3
 
+
+
 # adjacent_list = [(1,2), (2,3), (2,4), (3,4), (1, 3)]
 # vert = 4
+
+
+
+
 
 adjacent_list = [(1,2), (2,3), (2,4), (4,5), (4,6), (5,6), (4,7), (7,8), (7,9), (7,10), (8,9)]
 vert = 10
 
-print(combinatorial_optimization_MIS(adjacent_list, vert))
-
-        
-        
+print(graph_coloring(adjacent_list, vert))
+# print(combinatorial_optimization_MIS(adjacent_list, vert))
 
 
-        
