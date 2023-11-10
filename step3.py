be9fca16de1e866bfee981514fce0c8819d34d6b
Hugo <hugo.weiss@epita.fr>
2023-11-10 15:03:18 +0100

    adiabatic pulse for MIS

--- step3.py
+++ step3.py
@@ -3,13 +3,14 @@ import numpy as np
 from pprint import pprint
 from pulser import Pulse, Sequence, Register
 from pulser.devices import MockDevice
-from pulser.waveforms import RampWaveform, BlackmanWaveform, ConstantWaveform
+from pulser.waveforms import RampWaveform, BlackmanWaveform, ConstantWaveform, InterpolatedWaveform
 from pulser_simulation import QutipBackend
 
 
 def pulser_MIS(antennas_coordinates, distance, active_qubits=None):
     if active_qubits is None:
         active_qubits = list(range(len(antennas_coordinates)))
+    # Place the qubits correponding to the remaining uncolored edges
     active_coords = [antennas_coordinates[i] for i in active_qubits]
     reg = Register.from_coordinates(active_coords)
     #reg.draw(blockade_radius=distance, draw_half_radius=True, draw_graph=True)
@@ -19,22 +20,28 @@ def pulser_MIS(antennas_coordinates, distance, active_qubits=None):
     seq = Sequence(reg, MockDevice)
     seq.declare_channel('ch', 'rydberg_global')
 
-    omega_wf_1 = RampWaveform(3000, 0, omega)   
-    delta_wf_1 = ConstantWaveform(3000, -20)
-    pulse1 = Pulse(omega_wf_1, delta_wf_1, 0)
-    seq.add(pulse1, 'ch')
+    # omega_wf_1 = RampWaveform(3000, 0, omega)   
+    # delta_wf_1 = ConstantWaveform(3000, -20)
+    # pulse1 = Pulse(omega_wf_1, delta_wf_1, 0)
+    # seq.add(pulse1, 'ch')
 
 
-    omega_wf_2 = ConstantWaveform(10000, omega)  
-    delta_wf_2 = RampWaveform(10000, -20, 20) 
-    pulse2 = Pulse(omega_wf_2, delta_wf_2, 0)
-    seq.add(pulse2, 'ch')
+    # omega_wf_2 = ConstantWaveform(10000, omega)  
+    # delta_wf_2 = RampWaveform(10000, -20, 20) 
+    # pulse2 = Pulse(omega_wf_2, delta_wf_2, 0)
+    # seq.add(pulse2, 'ch')
 
 
-    omega_wf_3 = RampWaveform(3000, omega, 0)   
-    delta_wf_3 = ConstantWaveform(3000, 20)
-    pulse3 = Pulse(omega_wf_3, delta_wf_3, 0)
-    seq.add(pulse3, 'ch')
+    # omega_wf_3 = RampWaveform(3000, omega, 0)   
+    # delta_wf_3 = ConstantWaveform(3000, 20)
+    # pulse3 = Pulse(omega_wf_3, delta_wf_3, 0)
+    # seq.add(pulse3, 'ch')
+
+    omega_wf = InterpolatedWaveform(10000, [0, omega, 0])
+    delta_wf = InterpolatedWaveform(10000, [-20, 0, 20])
+    pulse = Pulse(omega_wf, delta_wf, 0)
+    seq.add(pulse, 'ch')
+
 
     sim= QutipBackend(seq)
     results=sim.run()
@@ -52,6 +59,6 @@ def pulser_MIS(antennas_coordinates, distance, active_qubits=None):
     for i in range(len(full_bitstring)):
         if full_bitstring[i] == '1':
             mis.append(i+1)
-    print("Maximum Independent Set23434234:", mis)
+    print("Maximum Independent Set:", mis)
 
     return ''.join(full_bitstring)
